9c4d4cbf47ceefc94217d0cc29cd2a70dc9b7bbc
Thang <pxthang97@gmail.com>
2021-11-26 16:24:48 +0700

    setup ok

--- internal/app/model/product.go
+++ internal/app/model/product.go
@@ -7,7 +7,30 @@ type (
 		URL  string `json:"url"`
 	}
 
-	MessgaeSendDataload struct {
+	RawProduct struct {
+		SKU         string `json:"sku"`
+		Description string `json:"description"`
+		Slugname    string `json:"slug_name"`
+		Link        string `json:"link"`
+		EcProductId string `json:"ec_product_id"`
+		CateId      string `json:"category_id"`
+		Title       string `json:"title"`
+		VendorId    string `json:"vendor_id"`
+		MadeIn      string `json:"made_in"`
+		Detail      string `json:"detail"`
+		Variant     Variant
+	}
+
+	Variant struct {
+		Id            string   `json:"id"`
+		Price         float64  `json:"Price"`
+		DiscountPrice float64  `json:"discount_price"`
+		Name          string   `json:"name"`
+		Color         string   `json:"color"`
+		Size          string   `json:"size"`
+		Images        []string `json:"images"`
+		Videos        []string `json:"videos"`
+		Stock         int      `json:"stock"`
 	}
 
 	HealthCheckResponse struct {
